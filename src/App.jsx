a63cbe20aab511f15139177ce55581bdb1f0f4a8
dieppciiuu <diephan161@gmail.com>
2026-04-01 03:48:25 +0700

    update UI + chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import React from "react";
 import ChatBox from "./components/ChatBox";
+import VoiceBox from "./components/VoiceBox";
 
 function App() {
   return (
-    <div>
+    <div className="relative w-screen h-screen bg-gray-50">
+      {/* Chat Box */}
       <ChatBox />
+
+      {/* Voice Box */}
+      <VoiceBox />
     </div>
   );
 }
