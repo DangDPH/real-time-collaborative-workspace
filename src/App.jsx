50e984b7f05fb6e10625469b68c9daefaa06edb9
Anh Duong <nguyetheduong81129@gmail.com>
2026-04-01 11:30:37 +0700

    Fix merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Whiteboard from "./components/canvas/white_board";
-import ChatBox from "./components/ChatBox";
-import VoiceBox from "./components/VoiceBox";
+import React from 'react';
+import Whiteboard from './components/canvas/white_board';
+import ChatBox from './components/ChatBox';
+import VoiceBox from './components/VoiceBox';
 
 function App() {
   return (
     <div className="relative w-screen h-screen bg-gray-50">
-      {/* Whiteboard nền */}
+      {/* Whiteboard làm nền phía dưới */}
       <Whiteboard />
 
-      {/* Chat Box (overlay) */}
-      <div className="absolute bottom-4 right-4">
+      {/* Chat Box nổi phía trên góc phải dưới */}
+      <div className="absolute bottom-4 right-4" style={{ zIndex: 1000 }}>
         <ChatBox />
       </div>
 
-      {/* Voice Box (overlay) */}
-      <div className="absolute bottom-20 right-4">
+      {/* Voice Box nổi phía trên Chat Box */}
+      <div className="absolute bottom-20 right-4" style={{ zIndex: 1000 }}>
         <VoiceBox />
       </div>
     </div>
